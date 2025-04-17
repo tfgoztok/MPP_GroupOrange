47e492bf6712fa9488de6d946da1ca97ee613e15
Elijah Ariyibi <ayomideariyibi1975@gmail.com>
2025-04-17 11:39:15 -0500

    Prob2&3

--- Lab10/Prob2/PartA/Comparator2Fix.java
+++ Lab10/Prob2/PartA/Comparator2Fix.java
@@ -1,4 +1,49 @@
 package Lab10.Prob2.PartA;
 
-public class Comparator2Fix {
+
+import java.util.*;
+
+class Employee {
+    String name;
+    double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Employee)) return false;
+        Employee e = (Employee) o;
+        return name.equals(e.name) && salary == e.salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + salary;
+    }
 }
+
+class EmployeeNameComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        int nameComp = e1.name.compareTo(e2.name);
+        if (nameComp != 0) return nameComp;
+        return Double.compare(e1.salary, e2.salary);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        List<Employee> employees = Arrays.asList(
+                new Employee("Joe", 150000),
+                new Employee("Joe", 100000),
+                new Employee("Anna", 120000)
+        );
+
+        Collections.sort(employees, new EmployeeNameComparator());
+        employees.forEach(System.out::println);
+    }
+}
+
--- Lab10/Prob2/PartB/Comparator3Fix.java
+++ Lab10/Prob2/PartB/Comparator3Fix.java
@@ -1,4 +1,60 @@
 package Lab10.Prob2.PartB;
 
-public class Comparator3Fix {
+
+
+import java.util.*;
+
+class Employee {
+    String name;
+    double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Employee)) return false;
+        Employee e = (Employee) o;
+        return name.equals(e.name) && salary == e.salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + salary;
+    }
 }
+
+class EmployeeInfo {
+    enum SortMethod { BYNAME, BYSALARY }
+
+    public void sort(List<Employee> emps, final SortMethod method) {
+        class EmployeeComparator implements Comparator<Employee> {
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                if (method == SortMethod.BYNAME) {
+                    int nameComp = e1.name.compareTo(e2.name);
+                    if (nameComp != 0) return nameComp;
+                    return Double.compare(e1.salary, e2.salary);
+                } else {
+                    return Double.compare(e1.salary, e2.salary);
+                }
+            }
+        }
+
+        Collections.sort(emps, new EmployeeComparator());
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employees = Arrays.asList(
+                new Employee("Joe", 150000),
+                new Employee("Joe", 100000),
+                new Employee("Anna", 120000)
+        );
+
+        new EmployeeInfo().sort(employees, SortMethod.BYNAME);
+        employees.forEach(System.out::println);
+    }
+}
+
